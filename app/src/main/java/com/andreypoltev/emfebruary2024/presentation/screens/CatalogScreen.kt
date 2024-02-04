4513c9239ea306a4ef8bb1bf6919e0438b4216b6
andreypoltev <andreypoltev@gmail.com>
2024-02-04 16:29:35 +0300

    Tag layout changes

--- app/src/main/java/com/andreypoltev/emfebruary2024/presentation/screens/CatalogScreen.kt
+++ app/src/main/java/com/andreypoltev/emfebruary2024/presentation/screens/CatalogScreen.kt
@@ -3,16 +3,26 @@ package com.andreypoltev.emfebruary2024.presentation.screens
 import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.andreypoltev.emfebruary2024.MainViewModel
@@ -21,7 +31,7 @@ import com.andreypoltev.emfebruary2024.Tag
 import com.andreypoltev.emfebruary2024.presentation.composables.CustomProgressIndicator
 import com.andreypoltev.emfebruary2024.presentation.composables.ItemsGrid
 import com.andreypoltev.emfebruary2024.presentation.composables.SortDropdownMenu
-import com.andreypoltev.emfebruary2024.presentation.composables.TagFilterCard
+import com.andreypoltev.emfebruary2024.presentation.screens.catalog_screen_composables.TagFilterCard
 
 
 @Composable
@@ -40,13 +50,39 @@ fun CatalogScreen(viewModel: MainViewModel, navController: NavHostController) {
     if (itemsFilteredAndSorted.isNotEmpty()) {
         Column {
 
-            val isExpanded = remember { mutableStateOf(false) }
+            Row {
 
-            SortDropdownMenu(
-                viewModel = viewModel,
-                onClick = { isExpanded.value = !isExpanded.value },
-                isExpanded = isExpanded
-            ) { isExpanded.value = false }
+
+                val isExpanded = remember { mutableStateOf(false) }
+
+                SortDropdownMenu(
+                    viewModel = viewModel,
+                    onClick = { isExpanded.value = !isExpanded.value },
+                    isExpanded = isExpanded
+                ) { isExpanded.value = false }
+
+
+                Spacer(modifier = Modifier.weight(1f))
+
+                Card(
+                    onClick = {},
+                    colors = CardDefaults.cardColors(containerColor = colorResource(id = R.color.background_white))
+                ) {
+
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+
+                        Icon(
+                            painter = painterResource(id = R.drawable.filter_icon),
+                            contentDescription = null
+                        )
+
+                        Text(
+                            modifier = Modifier.padding(8.dp),
+                            text = stringResource(id = R.string.filters)
+                        )
+                    }
+                }
+            }
 
             LazyRow(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
 
---
+++ app/src/main/java/com/andreypoltev/emfebruary2024/presentation/screens/catalog_screen_composables/TagFilterCard.kt
@@ -0,0 +1,68 @@
+package com.andreypoltev.emfebruary2024.presentation.screens.catalog_screen_composables
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.andreypoltev.emfebruary2024.MainViewModel
+import com.andreypoltev.emfebruary2024.R
+import com.andreypoltev.emfebruary2024.Tags
+
+@Composable
+fun TagFilterCard(viewModel: MainViewModel, resourceId: Int, tag: String, onClick: () -> Unit) {
+
+    val currentTag by viewModel.filterTag.collectAsState()
+
+    Card(
+        onClick = onClick,
+        shape = RoundedCornerShape(100.dp), colors = CardDefaults.cardColors(
+            containerColor = colorResource(
+                id = if (currentTag == tag) R.color.element_dark_blue else R.color.background_light_grey
+            )
+        )
+    ) {
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                color = colorResource(id = if (currentTag == tag) R.color.text_white else R.color.text_grey),
+                text = stringResource(id = resourceId),
+                modifier = if (currentTag == tag) Modifier.padding(
+                    start = 12.dp,
+                    end = 4.dp,
+                    top = 4.dp,
+                    bottom = 4.dp
+                ) else Modifier.padding(horizontal = 12.dp, vertical = 4.dp)
+            )
+
+            if (currentTag == tag) {
+                Icon(
+                    modifier = Modifier
+                        .padding(end = 8.dp)
+                        .clickable { viewModel.setTag(Tags.showAll.tag) },
+                    painter = painterResource(id = R.drawable.remove_tag_icon),
+                    tint = colorResource(id = R.color.element_white),
+                    contentDescription = null
+                )
+
+            }
+
+        }
+
+
+    }
+
+}
